98ce1e03ce43ccc50983cf0490e0a65cedd2da71
anashm <anashammaoui07@gmail.com>
2024-11-09 15:22:34 +0100

    ch6

---
+++ ch6.js
@@ -0,0 +1,8 @@
+function getlongword(word1, word2) {
+    if (word1.length > word2.length){
+        console.log(word1);
+    } else {
+        console.log(word2);
+    }
+}
+getlongword("cat","mouse");
